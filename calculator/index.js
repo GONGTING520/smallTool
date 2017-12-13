d1ad3f6d08534d45edc73b89fe9b82a73b2f7673
GONGTING520 <954160311@qq.com>
2017-12-13 17:08:09 +0800

    当多次点击运算符时，不会重复出现，而是替换已经输入的，若没有输入则添加

--- calculator/index.js
+++ calculator/index.js
@@ -1,19 +1,33 @@
 var aTd = document.getElementsByTagName('td');
-var oResult;//定义显示结果的td
-for (var i = 0 ; i < aTd.length; i++) {
-	if(aTd[i].className === 'result'){
-		oResult=aTd[i];
-	}else if(aTd[i].className === 'cal'){
-		aTd[i].onclick = function(){
+var oResult; //定义显示结果的td
+for (var i = 0; i < aTd.length; i++) {
+	if (aTd[i].className === 'result') {
+		oResult = aTd[i];
+		oResult.bFlag = false; //false表示还未点击运算符，true表示点击了
+	} else if (aTd[i].className === 'cal') { //=
+		aTd[i].onclick = function () {
 			oResult.innerHTML = eval(oResult.innerHTML);
+			oResult.bFlag = false;
 		}
-	}else if(aTd[i].className === 'clear'){
-		aTd[i].onclick = function(){
+	} else if (aTd[i].className === 'clear') { //C
+		aTd[i].onclick = function () {
 			oResult.innerHTML = '';
+			oResult.bFlag = false;
 		}
-	}else{
-		aTd[i].onclick = function(){
+	} else if (aTd[i].className === 'operator') {
+		aTd[i].onclick = function () {
+			if (!oResult.bFlag) {
+				oResult.innerHTML += this.innerHTML;
+				oResult.bFlag = true;
+			} else {
+				var oReInner = oResult.innerHTML;
+				oResult.innerHTML = oReInner.substring(0, oReInner.length - 1) + this.innerHTML;
+			}
+		}
+	} else {
+		aTd[i].onclick = function () {
 			oResult.innerHTML += this.innerHTML;
+			oResult.bFlag = false;
 		};
 	}
 }
